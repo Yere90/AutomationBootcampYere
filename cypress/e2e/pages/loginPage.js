0679949d82a5886e73d4fd2b2ccec6ee2808c4c9
Javier Flores <javi.flores@plytix.com>
2024-11-04 22:57:11 +0100

    Sugerencias Yere

--- cypress/e2e/pages/loginPage.js
+++ cypress/e2e/pages/loginPage.js
@@ -2,61 +2,25 @@
 export class LoginPage {
     
 
-    checkNotIncludeProperty(notincludeProperty, endpoint) {
-        cy.url().should(notincludeProperty, endpoint);
-    }
+  checkNotIncludeProperty(notincludeProperty, endpoint) {
+    cy.url().should(notincludeProperty, endpoint);
+  }
 
-    validLogin(userName, password) {
-        cy.get('[data-test="username"]')
-            .should('have.value', '')
-            .type(userName)
-            .should('have.value', userName);
-        cy.get('[data-test="password"]')
-            .should('have.value', '')
-            .type(password)
-            .should('have.value', password);
-        cy.get('[data-test="login-button"]').click();
-    }
+  validLogin(userName, password) {
+    cy.get('[data-test="username"]')
+        .should('have.value', '')
+        .type(userName)
+        .should('have.value', userName);
+    cy.get('[data-test="password"]')
+        .should('have.value', '')
+        .type(password)
+        .should('have.value', password);
+    cy.get('[data-test="login-button"]').click();
+  }
 
-    checkIncludeProperty(includeProperty, url) {
-        cy.url().should(includeProperty, url);
-    }
-
-    // checkActiveOption(selectedOption) {
-    //     cy.get('select[data-test="product_sort_container"]')
-    //       .should('exist')
-    //       .should('be.visible')
-    //       .find('option:selected')
-    //       .should('have.text', selectedOption);
-    // }
-
-    checkNameFirstProduct(nameFirstProduct) {
-        cy.get('[data-test="inventory-item-name"]')
-          .contains(nameFirstProduct);
-
-    }
-
-    checkPriceFirstProduct(priceFirstProduct) {
-        cy.get('[data-test="inventory-item-price"]')
-          .contains(priceFirstProduct);
-    }     
-
-    checkNameLastProduct(nameLastProduct) {
-        cy.get('[data-test="inventory-item-name"]').last()
-          .contains(nameLastProduct);
-    
-    }
-
-    checkPriceLastProduct(priceLastProduct) {
-        cy.get('[data-test="inventory-item-price"]').last()
-          .contains(priceLastProduct);
-
-    }
-
-    selectOption(option) {
-        cy.get('[data-test="product-sort-container"]')
-          .select(option);
-    }
+  checkIncludeProperty(includeProperty, url) {
+    cy.url().should(includeProperty, url);
+  }
 
 }
 
--- cypress/e2e/step-definitions/mainSteps.js
+++ cypress/e2e/step-definitions/mainSteps.js
@@ -18,3 +18,75 @@ When("I add to the cart the first product in the list",() => {
 When("I check that the shopping cart icon contains the number {int}",(number) => {
   mainPage.checkCartIconBadgeContainNumber(number)
 });
+
+Then("I check that the select has {string} as the selected option", (selectedOption) => {
+  mainPage.checkActiveOption(selectedOption);
+});
+
+Then ("I check that the first product on the list is called {string}", (nameFirstProduct) => {
+  mainPage.checkNameFirstProduct(nameFirstProduct);
+}); 
+
+Then("I check that the first product in the list has a price of {float}", (priceFirstProduct) => {
+  mainPage.checkPriceFirstProduct(priceFirstProduct);
+});
+
+Then("I check that the last product in the list is called {string}", (nameLastProduct) => {
+  mainPage.checkNameLastProduct(nameLastProduct);
+});
+
+Then("I check the last product on the list has a price of {float}", (priceLastProduct) => {
+  mainPage.checkPriceLastProduct(priceLastProduct);
+});
+
+Then ("I select the {string} option", (option) => {
+  mainPage.selectOption(option);
+});
+
+Then ("I check that the first product on the list is {string}", (firstProduct) => {
+  mainPage.checkNameFirstProduct(firstProduct);
+});
+
+Then("I check that the first product on the list has the price {float}", (priceProduct) => {
+  mainPage.checkPriceFirstProduct(priceProduct);
+});
+
+Then("I check that the last product on the list is {string}", (lastProductName) => {
+  mainPage.checkNameLastProduct(lastProductName);
+});
+
+Then ("I check the last product on the list cost {float}", (lastProductPrice) => {
+  mainPage.checkPriceLastProduct(lastProductPrice);
+});
+
+Then("I select the option {string}", (selectOption) => {
+  mainPage.selectOption(selectOption);
+});
+
+Then("I check the first product is {string}", (product) => {
+  mainPage.checkNameFirstProduct(product);
+});
+
+Then("I check the first product cost {float}", (price) => {
+  mainPage.checkPriceFirstProduct(price);
+});
+
+Then("I check the last product is {string}", (lastProduct) => {
+  mainPage.checkNameLastProduct(lastProduct);
+});
+
+Then("I check the last product cost {float}", (lastPrice) => {
+  mainPage.checkPriceLastProduct(lastPrice);
+});
+
+When("I click on the button {string}",(buttonName) => {
+  mainPage.clickButtonByDataTest(buttonName)
+});
+
+When("I check that the element {string} should {string}",(elementDataTest, status) => {
+  mainPage.checkStatusOnElementByDataTest(elementDataTest, status)
+});
+
+When("I check that the element {string} should {string} the content {string}",(elementDataTest, statusContent, content) => {
+  mainPage.checkElementContent(elementDataTest, statusContent, content)
+});
